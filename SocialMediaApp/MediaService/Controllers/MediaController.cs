38e078d596d4e67dbe6cc3683106db885f04ea6f
rahimetasdemir <rhmtsdmr@gmail.com>
2023-07-03 02:29:12 +0300

    media service added.

---
+++ SocialMediaApp/MediaService/Controllers/MediaController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MediaService.Controllers
+{
+   // [Route("api/[controller]")]
+    [ApiController]
+    public class MediaController : ControllerBase
+    {
+        [HttpPost]
+        [Route("UploadFile")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+
+        public async Task<IActionResult> UploadFile(IFormFile file, CancellationToken cancellationtoken)
+        {
+            var result = await WriteFile(file);
+            return Ok(result);
+        }
+
+        private async Task<string> WriteFile(IFormFile file)
+        {
+            string filename = "";
+            try
+            {
+                var extension = "." + file.FileName.Split('.')[file.FileName.Split('.').Length - 1];
+                filename = DateTime.Now.Ticks.ToString() + extension;
+
+                var filepath = Path.Combine(Directory.GetCurrentDirectory(), "Upload\\MediaFiles", filename);
+
+                if(!Directory.Exists(filepath))
+                {
+                    Directory.CreateDirectory(filepath);
+                }
+                var exactpath = Path.Combine(Directory.GetCurrentDirectory(), "Upload\\MediaFiles", filename);
+                using (var stream = new FileStream(exactpath, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            return filename;
+        }
+
+    }
+}

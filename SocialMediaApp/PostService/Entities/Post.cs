143a49f291470a75383492af68ef0ba4e78284f4
Zeynep Derbent <57301676+zeynep394@users.noreply.github.com>
2023-07-06 20:20:54 +0300

    Update Post.cs

--- SocialMediaApp/PostService/Entities/Post.cs
+++ SocialMediaApp/PostService/Entities/Post.cs
@@ -11,7 +11,7 @@ namespace PostService.Entities
         public int Id { get; set; }
 
 
-        public int UserId { get; set; }
+        public string Username { get; set; }
 
 
         public string Description { get; set; }

63d4ad304650601e3225b0995767102bcc59927d
Ahmet Oğuzperdahçı <ahmetoguzperdahci@gmail.com>
2023-07-06 19:10:40 +0300

    feat(UserService): add profile photo to user

--- SocialMediaApp/UserService/Models/UserDTO.cs
+++ SocialMediaApp/UserService/Models/UserDTO.cs
@@ -5,5 +5,6 @@
         public string Username { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+        public string ProfilePhoto { get; set; }
     }
 }
